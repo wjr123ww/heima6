c36f0db1a32a832bf18099940e3bcab511dedd13
wjr123ww <1094342057@qq.com>
2020-07-03 16:15:50 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'amfe-flexible'
+import './styles/common.less'
+import './styles/iconfont.less'
+import HmHeader from './components/HmHeader.vue'
+import Hmlogo from './components/HmLogo.vue'
+import { Button, Field, Form } from 'vant'
 
+Vue.use(Button)
+Vue.use(Field)
+Vue.use(Form)
+Vue.component('hm-header', HmHeader)
+Vue.component('hm-logo', Hmlogo)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <hm-header>登录</hm-header>
+    <hm-logo></hm-logo>
+    <van-form>
+      <van-field
+        v-model="username"
+        label="用户名"
+        placeholder="请输入用户名/手机号"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        label="密码"
+        placeholder="请输入密码"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
